701c68817ddfaf9a0c842fa2978144693f98a51c
Obonyo Jimmycliff <cliffjimmy27@gmail.com>
2016-01-23 02:20:52 +0300

    First Commit

--- config/kopokopo.php
+++ config/kopokopo.php
@@ -4,32 +4,5 @@
 
 return [
  'service_name' => 'M-PESA',
- 
- 'business_number' => '888555',
- 
-// 'transaction_reference' => '',
- 
- 'internal_transaction_id' => '',
- 
- 'transaction_timestamp' => date('c'),
- 
- 'transaction_type' => 'Paybill',
- 
- 'account_number' => '',
- 
-// 'sender_phone' => '',
- 
-// 'first_name' => '',
- 
-// 'middle_name' => '',
- 
-// 'last_name' => '',
- 
- 'amount' => '',
- 
- 'currency' => 'KES',
-  
-
-   
 
 ];
--- src/Contract/ApiHelper.php
+++ src/Contract/ApiHelper.php
@@ -7,29 +7,8 @@ use Obonyojimmy\Kopokopo_laravel\Contract\ApiContract;
 
 class ApiHelper implements ApiContract
 {
-     protected $service_name = config('kopokopo.service_name');
-     protected $business_number = config('kopokopo.business_number');
-   //  protected $transaction_reference = config('kopokopo.transaction_reference');
-     protected $internal_transaction_id = config('kopokopo.internal_transaction_id');
-     protected $transaction_timestamp = config('kopokopo.transaction_timestamp');
-     protected $transaction_type = config('kopokopo.transaction_type');
-     protected $account_number = config('kopokopo.account_number');
-   //  protected $sender_phone = config('kopokopo.sender_phone');
-   //  protected $first_name = config('kopokopo.first_name');
-   // protected $middle_name = config('kopokopo.middle_name');
-   //  protected $last_name = config('kopokopo.last_name');
-     protected $amount = config('kopokopo.amount');
-     protected $currency = config('kopokopo.currency');
-  
-      
-	 public  function gateway($transaction_reference,$sender_phone,$first_name,$middle_name,$last_name,$amount) 
-	{ 
-		$client = new GuzzleHttp\Client();
-		$res = $client->request('GET', 'https://api.github.com/user', [
-			'auth' => ['obonyojimmy', 'masenoc27']
-		]);
-		return $res->getBody();
-	} 
+      $service_name = config('kopokopo.service_name');
+    
 	 public  function test() 
 	{ 
 		$client = new GuzzleHttp\Client();
